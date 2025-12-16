8344e9349bc2d55e89c8eee3520ee6f211e92e60
Dmitry Konstantinov <netudima@gmail.com>
2025-12-16 13:58:30 +0000

    more microbenchmakrs

--- test/microbench/org/apache/cassandra/test/microbench/UTF8ValidatorBench.java
+++ test/microbench/org/apache/cassandra/test/microbench/UTF8ValidatorBench.java
@@ -28,10 +28,13 @@ import org.apache.cassandra.serializers.UTF8Serializer;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
@@ -45,11 +48,50 @@ import org.openjdk.jmh.annotations.Warmup;
 @State(Scope.Benchmark)
 public class UTF8ValidatorBench
 {
-    byte[] arrayValue = "ASCII string".getBytes(StandardCharsets.UTF_8);
-    ByteBuffer heapByteBufferValue = ByteBuffer.allocate(arrayValue.length);
+
+    @Param({ "short ASCII", "long ASCII", "short ASCII prefix non-ASCII", "short non-ASCII", "long non-ASCII"})
+    private String stringType;
+
+    byte[] arrayValue;
+    ByteBuffer heapByteBufferValue;
+
+    @Setup(Level.Trial)
+    public void setup() throws Throwable
     {
+        switch (stringType)
+        {
+            case "short ASCII":
+                arrayValue = "ASCII string".getBytes(StandardCharsets.UTF_8);
+                break;
+            case "long ASCII":
+                arrayValue = ("ASCII is an acronym for American Standard Code for Information Interchange, " +
+                              "is a character encoding standard for representing a particular set of 95 " +
+                              "(English language focused) printable and 33 control characters – a total of 128 code points. " +
+                              "The set of available punctuation had significant impact on the syntax of computer languages " +
+                              "and text markup. ASCII hugely influenced the design of character sets used by modern computers; " +
+                              "for example, the first 128 code points of Unicode are the same as ASCII.").getBytes(StandardCharsets.UTF_8);
+                break;
+            case "short ASCII prefix non-ASCII":
+                arrayValue = "a hierarchy of number systems: ℕ ⊆ ℕ₀ ⊂ ℤ ⊂ ℚ ⊂ ℝ ⊂ ℂ".getBytes(StandardCharsets.UTF_8);
+                break;
+            case "short non-ASCII":
+                arrayValue = "ℕ ⊆ ℕ₀ ⊂ ℤ ⊂ ℚ ⊂ ℝ ⊂ ℂ".getBytes(StandardCharsets.UTF_8);
+                break;
+            case "long non-ASCII": // https://www.w3.org/2001/06/utf-8-test/UTF-8-demo.html
+                arrayValue = ("⡍⠜⠇⠑⠹ ⠺⠁⠎ ⠙⠑⠁⠙⠒ ⠞⠕ ⠃⠑⠛⠔ ⠺⠊⠹⠲ ⡹⠻⠑ ⠊⠎ ⠝⠕ ⠙⠳⠃⠞\n" +
+                             "  ⠱⠁⠞⠑⠧⠻ ⠁⠃⠳⠞ ⠹⠁⠞⠲ ⡹⠑ ⠗⠑⠛⠊⠌⠻ ⠕⠋ ⠙⠊⠎ ⠃⠥⠗⠊⠁⠇ ⠺⠁⠎\n" +
+                             "  ⠎⠊⠛⠝⠫ ⠃⠹ ⠹⠑ ⠊⠇⠻⠛⠹⠍⠁⠝⠂ ⠹⠑ ⠊⠇⠻⠅⠂ ⠹⠑ ⠥⠝⠙⠻⠞⠁⠅⠻⠂\n" +
+                             "  ⠁⠝⠙ ⠹⠑ ⠡⠊⠑⠋ ⠍⠳⠗⠝⠻⠲ ⡎⠊⠗⠕⠕⠛⠑ ⠎⠊⠛⠝⠫ ⠊⠞⠲ ⡁⠝⠙\n" +
+                             "  ⡎⠊⠗⠕⠕⠛⠑⠰⠎ ⠝⠁⠍⠑ ⠺⠁⠎ ⠛⠕⠕⠙ ⠥⠏⠕⠝ ⠰⡡⠁⠝⠛⠑⠂ ⠋⠕⠗ ⠁⠝⠹⠹⠔⠛ ⠙⠑ \n" +
+                             "  ⠡⠕⠎⠑ ⠞⠕ ⠏⠥⠞ ⠙⠊⠎ ⠙⠁⠝⠙ ⠞⠕⠲").getBytes(StandardCharsets.UTF_8);
+                break;
+            default:
+                throw new UnsupportedOperationException();
+        }
+        heapByteBufferValue = ByteBuffer.allocate(arrayValue.length);
         heapByteBufferValue.put(arrayValue).rewind();
     }
+
 
     @Benchmark
     public void testNewBimorphic()
